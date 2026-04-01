3063f69c02f687e3297933f77cfcef3c28003931
Igor Shavlov <shavlov.igor1234@gmail.com>
2026-04-01 12:48:16 +1000

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -3,50 +3,40 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
 )
 
-// Artist описывает музыкальную группу.
-type Artist struct {
-	ID    int      `json:"id"`
-	Name  string   `json:"name"`
-	Genre string   `json:"genre"`
-	Songs []string `json:"songs"`
-}
-
-// переменная artists содержит пока один музыкальный коллектив
-var artists = map[string]Artist{
-	"30 seconds to Mars": {
-		ID:    1,
-		Name:  "30 seconds to Mars",
-		Genre: "rock",
-		Songs: []string{
-			`The Kill`,
-			`A Beautiful Lie`,
-			`Attack`,
-			`Live Like A Dream`,
-		},
-	},
-}
-
-// JSONHandler принимает значение из параметра id, ищет по нему в мапе группу, конвертирует
-// данные из переменной artists в JSON и выводит их в браузере.
-func JSONHandler(w http.ResponseWriter, r *http.Request) {
-	band := r.URL.Query().Get("id")
-	resp, err := json.Marshal(artists[band])
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(resp)
+type User struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"email,omitempty"`
+	Password string `json:"-"`
+	active   bool
 }
 
 func main() {
-	http.HandleFunc(`/`, JSONHandler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Printf("ошибка запуска сервера: %s\n", err.Error())
+	user := User{
+		ID:       1,
+		Name:     "Гофер",
+		Email:    "gopher@gophermate.com",
+		Password: "I4mG0ph3R",
+		active:   true,
+	}
+
+	// преобразуйте user в JSON формат
+	out, err := json.Marshal(user)
+	if err != nil {
+		fmt.Printf("ошибка при сериализации в json: %s\n", err.Error())
 		return
 	}
+
+	var newUser User
+
+	// десериаизуйте данные из JSON формата в переменную newUser
+	// ...
+	if err = json.Unmarshal(out, &newUser); err != nil {
+		fmt.Printf("ошибка при десериализации в json: %s\n", err.Error())
+		return
+	}
+
+	fmt.Println(newUser)
 }
